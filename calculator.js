d33aed8d6514e256a856dab19ef04df4baa48ad5
Andy Hua <user.andy.hua@gmail.com>
2022-09-07 16:29:23 -0700

    added + and -

--- calculator.js
+++ calculator.js
@@ -1,17 +1,33 @@
-let reset = 0;
-let value = 0;
-let equations = ["5", "x", "5", "x", "5", "="];
+let equations = [];
+
 let result = document.getElementById("result");
 let equation = document.getElementById("equation");
-equation.innerHTML = equations.join(" ");
-value = 125;
-result.innerHTML = String(value).length < 12 ? value : "Error";
+let numbers = document.getElementsByClassName("num");
+let equals = document.querySelector(".equals");
+let clear = document.querySelector(".clear");
+
+for (let number of numbers) {
+    number.addEventListener("click", function () {
+        equations.push(number.textContent);
+        console.log(number.textContent);
+        equation.innerHTML = equations.join(" ");
+    });
+}
+
+clear.addEventListener("click", allClear);
+equals.addEventListener("click", calculate);
+
+function calculate() {
+    equation.textContent = equations.join(" ") + " = ";
+    result.innerHTML = eval(equations.join(""));
+    equations = [];
+}
 
 function displayResult() {}
 
-function pushToEquations() {}
-
 function allClear() {
     result.innerHTML = 0;
+    value = 0;
     equation.innerHTML = "";
+    equations = [];
 }
