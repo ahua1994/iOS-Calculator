2c6ad8ad4db28af149cc87a869fe5323eb538151
Andy Hua <user.andy.hua@gmail.com>
2022-09-13 21:47:05 -0700

    pretty good

--- calculator.js
+++ calculator.js
@@ -1,36 +1,71 @@
-// let equations = [];
-
-let result = document.getElementById("result");
-// let equation = document.getElementById("equation");
-// let numbers = document.getElementsByClassName("num");
-// let equals = document.querySelector(".equals");
-// let clear = document.querySelector(".clear");
-
-// for (let number of numbers) {
-//     number.addEventListener("click", function () {
-//         equations.push(number.textContent);
-//         console.log(number.textContent);
-//         equation.innerHTML = equations.join(" ");
-//     });
-// }
-
-// clear.addEventListener("click", allClear);
-// equals.addEventListener("click", calculate);
-
-// function calculate() {
-//     equation.textContent = equations.join(" ") + " = ";
-//     result.innerHTML = eval(equations.join(""));
-//     equations = [];
-// }
-
-// function allClear() {
-//     result.innerHTML = 0;
-//     value = 0;
-//     equation.innerHTML = "";
-//     equations = [];
-// }
+let equation = document.querySelector(".equation");
+let result = document.querySelector(".result");
 let buttons = document.querySelector(".buttons");
 
+let operands = ["+", "-", "x", "÷", "/", "*"];
+let currentEquation = [];
+let current = [];
+
 buttons.addEventListener("click", (e) => {
-    console.log(e.target);
+    let btnType = e.target.classList;
+
+    if (btnType.contains("num") && current.length < 7) {
+        if (current.length == 1 && current[0] == 0) current = [];
+        current.push(e.target.textContent);
+    } else if (btnType.contains("oper")) {
+        if (currentEquation.length == 0 && current.length != 0) {
+            currentEquation[0] = current.join("");
+        } else if (currentEquation.length == 2) {
+            if (operands.includes(currentEquation[1])) {
+                if (current.length == 0) {
+                    currentEquation.pop();
+                } else {
+                    equation.textContent = eval(
+                        currentEquation.join(" ") + current.join("")
+                    );
+                    trunc();
+                    currentEquation[0] = equation.textContent;
+                }
+            }
+        }
+        getOperand(e);
+        equation.textContent = currentEquation
+            .join(" ")
+            .replace("/", "÷")
+            .replace("*", "x");
+        current = [];
+    } else if (btnType.contains("decimal") && !current.includes(".")) {
+        if (current.length != 0) current.push(".");
+    } else if (btnType.contains("clear")) {
+        equation.textContent = "";
+        result.textContent = "";
+        currentEquation = [];
+        current = [];
+    } else if (btnType.contains("sign") && !current[0] == 0) {
+        current[0] = parseInt(current[0]) * -1;
+    } else if (btnType.contains("equals")) {
+        equation.textContent = eval(
+            currentEquation.join(" ") + current.join("")
+        );
+        trunc();
+        currentEquation[0] = equation.textContent;
+        current = [];
+    }
+    result.textContent = current.length == 0 ? "0" : current.join("");
 });
+
+function trunc() {
+    equation.textContent =
+        equation.textContent.length > 14
+            ? equation.textContent.slice(0, 14)
+            : equation.textContent;
+}
+
+function getOperand(e) {
+    currentEquation[1] =
+        e.target.textContent == "÷"
+            ? "/"
+            : e.target.textContent == "x"
+            ? "*"
+            : e.target.textContent;
+}
